015077f8b33d973b388ea0946a0889a3e322137f
kshitijjaiswal2805 <133943598+kshitijjaiswal2805@users.noreply.github.com>
2023-05-19 16:14:48 +0530

    My first commit

---
+++ src/main/java/LocalRepo/C1.java
@@ -0,0 +1,8 @@
+package LocalRepo;
+
+public class C1 {
+	public static void main(String[] args) {
+		System.out.println("This is C1 class");
+	}
+
+}
---
+++ src/main/java/LocalRepo/C2.java
@@ -0,0 +1,9 @@
+package LocalRepo;
+
+public class C2 {
+	public static void main(String[] args) {
+		System.out.println("This is C2 class");
+	}
+
+
+}
